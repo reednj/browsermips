59ca5631c916b845bf9b360404c1f1421689c51a
Nathan Reed <reednj@gmail.com>
2008-09-03 14:00:28 +0800

    minor changes to footer

--- index.php
+++ index.php
@@ -68,6 +68,7 @@ function getRecentMips($dbconn)
 						$mipval = $mip['mips'];
 						$platform = $mip['platform'];
 						$browser = $mip['browser'];
+						
 						print "<div>$mipval - $name ($browser, $platform)</div>";
 					}
 				}
@@ -118,8 +119,7 @@ function getRecentMips($dbconn)
 
 </div>
 
-<div class='bm-sig'>Nathan Reed (c) 2008 | <a href='http://www.servralert.com'>servralert.com</a> | <a href='http://github.com/reednj/browsermips/tree/master'>Github</a></div>	
-
+<div class='bm-sig'>Nathan Reed (c) 2008 | <a href='http://www.servralert.com'>servralert.com</a> | <a href='http://popacular.com'>popacular.com</a> | <a href='http://github.com/reednj/browsermips/tree/master'>Github</a></div>	
 <?php if($_SERVER['SERVER_NAME'] != 'rybafile') { ?>
 	
 <script type="text/javascript">
